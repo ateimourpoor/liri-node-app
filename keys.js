c608b7831815a0f4dcfc8cfe0be34955a15fbe2f
arash <arash.teimourpoor@gmail.com>
2017-10-17 13:37:45 -0400

    Initial Commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: '<7qfwXQ1falS8wn1Cwp6tfv6jF>',
+    consumer_secret: '<XaR9vcyQNSNfo3RL1SdcoXoF4VBCWYuViQwi7G44Gzz6rrUDat>',
+    access_token_key: '<\t920326851150696450-82LPkrR9BWe0Qkv9IgljLxkFYjiHztD>',
+    access_token_secret: '<h0rirQIHBy3mMgOwu40yQCBteRAW6aGzMRRgBu25f0T4a>',
+}
+
+module.exports = twitterKeys;
